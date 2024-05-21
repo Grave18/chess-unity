46a9205cd4bf576b15c5d5251e70e8c0c52125d6
Grave18 <grave63@gmail.com>
2024-05-21 20:59:11 +0600

    Adjust camera position

--- Assets/Code/View/CameraController.cs
+++ Assets/Code/View/CameraController.cs
@@ -20,11 +20,11 @@ namespace View
         [SerializeField] private float maxDistance = 4f;
 
         [Header("Pitch")]
+        [SerializeField] private float yawRad;
+        [SerializeField] private float pitchRad;
         [SerializeField] private float minPitchRad = 0.1f;
         [SerializeField] private float maxPitchRad = 1f;
 
-        private float _yaw;
-        private float _pitch;
 
         private void Update()
         {
@@ -35,23 +35,23 @@ namespace View
 
             if (Input.GetButton("Fire2"))
             {
-                _yaw -= Input.GetAxis("Mouse X") * sensitivity * dt;
-                _pitch -= Input.GetAxis("Mouse Y") * sensitivity * dt;
+                yawRad -= Input.GetAxis("Mouse X") * sensitivity * dt;
+                pitchRad -= Input.GetAxis("Mouse Y") * sensitivity * dt;
             }
 
-            _yaw += Input.GetAxis("Horizontal") * sensitivity * dt;
-            _pitch += Input.GetAxis("Vertical") * sensitivity * dt;
+            yawRad += Input.GetAxis("Horizontal") * sensitivity * dt;
+            pitchRad += Input.GetAxis("Vertical") * sensitivity * dt;
 
-            _pitch = Mathf.Clamp(_pitch, minPitchRad, maxPitchRad);
+            pitchRad = Mathf.Clamp(pitchRad, minPitchRad, maxPitchRad);
 
             // Scroll
             distance -= Input.GetAxis("Scroll") * scrollSensitivity * dt;
             distance -= Input.GetAxis("Mouse ScrollWheel") * scrollSensitivity;
             distance = Mathf.Clamp(distance, minDistance, maxDistance);
 
-            pos.x = distance * Mathf.Cos(_yaw) * Mathf.Cos(_pitch);
-            pos.y = distance * Mathf.Sin(_pitch);
-            pos.z = distance * Mathf.Sin(_yaw) * Mathf.Cos(_pitch);
+            pos.x = distance * Mathf.Cos(yawRad) * Mathf.Cos(pitchRad);
+            pos.y = distance * Mathf.Sin(pitchRad);
+            pos.z = distance * Mathf.Sin(yawRad) * Mathf.Cos(pitchRad);
 
             transform.position = pos;
 
