f7faf6a7611a66f1db77aabf0f8dcaee6c891a0e
Valerii Barabanov <grave63@gmail.com>
2025-01-26 20:45:07 +0300

    Reimport pieces and add dissolve effect

---
+++ Assets/Dissolve.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Dissolve : MonoBehaviour
+{
+    [SerializeField] private float dissolveTime = 1f;
+
+    private static readonly int TimeId = Shader.PropertyToID("_T");
+    private static readonly int SeedId = Shader.PropertyToID("_Seed");
+
+    private MeshRenderer _meshRenderer;
+    private Material _material;
+
+    private void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _material = _meshRenderer.material;
+        _material.SetFloat(SeedId, UnityEngine.Random.Range(0f, 100f));
+
+        StartCoroutine(Appear());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            StartCoroutine(Appear());
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            StartCoroutine(Disappear());
+        }
+    }
+
+    private IEnumerator Appear()
+    {
+        float time = 0f;
+        while (time <= dissolveTime)
+        {
+            time += Time.deltaTime;
+            float calculatedTime = Mathf.Lerp(0.001f, 1f, time / dissolveTime);
+            _material.SetFloat(TimeId, calculatedTime);
+            yield return null;
+        }
+    }
+
+    private IEnumerator Disappear()
+    {
+        float time = 0f;
+        while (time <= dissolveTime)
+        {
+            time += Time.deltaTime;
+            float calculatedTime = Mathf.Lerp(1f, 0.001f, time / dissolveTime);
+            _material.SetFloat(TimeId, calculatedTime);
+            yield return null;
+        }
+    }
+}
