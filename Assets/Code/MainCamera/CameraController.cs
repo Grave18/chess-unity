e92f488e0dc8766812caaeb0273ddb26d3d1d34c
Valerii Barabanov <grave63@gmail.com>
2025-04-18 14:10:31 +0300

    Upgrade camera behaviour

--- Assets/Code/MainCamera/CameraController.cs
+++ Assets/Code/MainCamera/CameraController.cs
@@ -1,40 +1,53 @@
 ﻿using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace MainCamera
 {
+    [RequireComponent(typeof(Camera))]
     public class CameraController : MonoBehaviour
     {
-        [Tooltip("Can be null if you want to look at the world origin")]
-        [SerializeField] private Transform target;
+        [Header("Target")]
+        [SerializeField] private Vector3 offset = new(0, 0.15f, 0);
 
-        [Header("Mouse")]
         [Header("Zoom")]
         [Tooltip("Camera distance from the target")]
-        [SerializeField] private float distance = 3.5f;
         [SerializeField] private float minDistance = 1f;
         [SerializeField] private float maxDistance = 4f;
-        [SerializeField] private float zoomScrollWheelSensitivity = 2.5f;
-        [SerializeField] private float zoomKeyboardSensitivity = 0.2f;
+        [SerializeField] private float distance = 3.5f;
+        [SerializeField] private float zoomSensitivity = 2.5f;
         [SerializeField] private float zoomSmoothTime = 0.15f;
 
         [Header("Orbiting")]
-        [SerializeField] private float yawRad;
-        [SerializeField] private float pitchRad;
         [SerializeField] private float minPitchRad = 0.1f;
         [SerializeField] private float maxPitchRad = 1f;
-        [SerializeField] private float orbitSensitivity = 10f;
-        [SerializeField] private float yawSmoothTime = 0.15f;
-        [SerializeField] private float pitchSmoothTime = 0.15f;
+        [SerializeField] private float pitchRad = 0.125f;
+        [SerializeField] private float yawRad = -1.5708f;
+        [SerializeField] private float orbitSensitivity = 0.2f;
+        [SerializeField] private float yawSmoothTime = 0.08f;
+        [SerializeField] private float pitchSmoothTime = 0.08f;
+
+        [Header("Fov")]
+        [SerializeField] private float fovStart = 60f;
+        [SerializeField] private float fovEnd = 30f;
+        [SerializeField] private float distanceFovMultEnd = 1.5f;
 
         private float _zoomCurrentVelocity;
         private float _yawCurrentVelocity;
         private float _pitchCurrentVelocity;
         private float _newDistance;
+        private float _distanceMult;
         private float _newYawRad;
         private float _newPitchRad;
+        private Vector3 _newPosition;
+        private float _t;
 
-        private void Start()
+        private Camera _camera;
+
+        private void Awake()
         {
+            _camera = GetComponent<Camera>();
+            Assert.IsNotNull(_camera);
+
             _newDistance = distance;
             _newYawRad = yawRad;
             _newPitchRad = pitchRad;
@@ -42,9 +55,33 @@ namespace MainCamera
 
         private void Update()
         {
-            // Position
-            Vector3 pos = transform.position;
+            CalculateTFromPitch();
+            CalculateZoom();
+            CalculateOrbit();
+            CalculateCameraPosition();
+            ApplyToCamera();
+        }
 
+        /// t = min pitch -> 0 max pitch -> 1
+        private void CalculateTFromPitch()
+        {
+            _t = (pitchRad - minPitchRad) / (maxPitchRad - minPitchRad);
+        }
+
+        private void CalculateZoom()
+        {
+            // From keyboard
+            _newDistance -= Input.GetAxis("Scroll") * zoomSensitivity;
+            // From scroll wheel
+            _newDistance -= Input.GetAxis("Mouse ScrollWheel") * zoomSensitivity;
+            _newDistance = Mathf.Clamp(_newDistance, minDistance, maxDistance);
+
+            // Smoothing zoom
+            distance = Mathf.SmoothDamp(distance, _newDistance, ref _zoomCurrentVelocity, zoomSmoothTime);
+        }
+
+        private void CalculateOrbit()
+        {
             if (Input.GetButton("Fire2"))
             {
                 _newYawRad -= Input.GetAxis("Mouse X") * orbitSensitivity;
@@ -58,32 +95,25 @@ namespace MainCamera
             // Smoothing orbit
             yawRad = Mathf.SmoothDamp(yawRad, _newYawRad, ref _yawCurrentVelocity, yawSmoothTime);
             pitchRad = Mathf.SmoothDamp(pitchRad, _newPitchRad, ref _pitchCurrentVelocity, pitchSmoothTime);
+        }
 
-            // Zoom
-            // From keyboard
-            _newDistance -= Input.GetAxis("Scroll") * zoomKeyboardSensitivity;
-            // From scroll wheel
-            _newDistance -= Input.GetAxis("Mouse ScrollWheel") * zoomScrollWheelSensitivity;
-            _newDistance = Mathf.Clamp(_newDistance, minDistance, maxDistance);
+        private void CalculateCameraPosition()
+        {
+            // Less fov more distance from target to camera
+            _distanceMult = Mathf.Lerp(1f, distanceFovMultEnd, _t);
+            float currentDistance = distance * _distanceMult;
 
-            // Smoothing zoom
-            distance = Mathf.SmoothDamp(distance, _newDistance, ref _zoomCurrentVelocity, zoomSmoothTime);
+            // Position
+            _newPosition.x = currentDistance * Mathf.Cos(yawRad) * Mathf.Cos(pitchRad) + offset.x;
+            _newPosition.y = currentDistance * Mathf.Sin(pitchRad) + offset.y;
+            _newPosition.z = currentDistance * Mathf.Sin(yawRad) * Mathf.Cos(pitchRad) + offset.z;
+        }
 
-            pos.x = distance * Mathf.Cos(yawRad) * Mathf.Cos(pitchRad);
-            pos.y = distance * Mathf.Sin(pitchRad);
-            pos.z = distance * Mathf.Sin(yawRad) * Mathf.Cos(pitchRad);
-
-            transform.position = pos;
-
-            // Rotation
-            if (target != null)
-            {
-                transform.LookAt(target);
-            }
-            else
-            {
-                transform.LookAt(Vector3.zero);
-            }
+        private void ApplyToCamera()
+        {
+            transform.position = _newPosition;
+            transform.LookAt(offset);
+            _camera.fieldOfView = Mathf.Lerp(fovStart, fovEnd, _t);
         }
     }
 }
