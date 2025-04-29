9c181009976d629534a4d7ccc146d43e68eddd50
Valerii Barabanov <grave63@gmail.com>
2025-04-29 13:37:22 +0300

    Add custom build tool

---
+++ Assets/Code/Tools/BuildTool/BuildSettings.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+using EditorCools;
+using UnityEditor;
+using UnityEngine;
+
+namespace Tools.BuildTool
+{
+    [CreateAssetMenu(menuName = "Build/Build Settings", fileName = "BuildSettings")]
+    public class BuildSettings : ScriptableObject
+    {
+        [Header("Build Settings")]
+        public string BuildPath;
+        public BuildTargetGroup BuildTargetGroup = BuildTargetGroup.Standalone;
+        public BuildTarget BuildTarget = BuildTarget.StandaloneWindows64;
+        public BuildOptions BuildOptions;
+
+        [Header("Development")]
+        [SerializeField] private bool developmentBuild;
+        [SerializeField] private bool allowDebugging;
+
+        [Header("Profiler")]
+        [SerializeField] private bool autoConnectProfiler;
+        [SerializeField] private bool deepProfiling;
+        [SerializeField] private bool waitForPlayerConnection;
+
+        [Header("Build")]
+        [SerializeField] private bool showBuildFolder;
+        [SerializeField] private bool autoRunPlayer;
+        [SerializeField] private bool clanBuild;
+
+        private void Reset()
+        {
+            BuildPath = Path.Combine($"{Application.dataPath}", "..", "Build");
+            BuildPath = Path.GetFullPath(BuildPath);
+        }
+
+        private void OnValidate()
+        {
+            BuildOptions = developmentBuild ? BuildOptions | BuildOptions.Development : BuildOptions & ~BuildOptions.Development;
+            BuildOptions = allowDebugging ? BuildOptions | BuildOptions.AllowDebugging : BuildOptions & ~BuildOptions.AllowDebugging;
+            BuildOptions = autoConnectProfiler ? BuildOptions | BuildOptions.ConnectWithProfiler : BuildOptions & ~BuildOptions.ConnectWithProfiler;
+            BuildOptions = deepProfiling ? BuildOptions | BuildOptions.EnableDeepProfilingSupport : BuildOptions & ~BuildOptions.EnableDeepProfilingSupport;
+            BuildOptions = waitForPlayerConnection ? BuildOptions | BuildOptions.WaitForPlayerConnection : BuildOptions & ~BuildOptions.WaitForPlayerConnection;
+            BuildOptions = autoRunPlayer ? BuildOptions | BuildOptions.AutoRunPlayer : BuildOptions & ~BuildOptions.AutoRunPlayer;
+            BuildOptions = showBuildFolder ? BuildOptions | BuildOptions.ShowBuiltPlayer : BuildOptions & ~BuildOptions.ShowBuiltPlayer;
+            BuildOptions = clanBuild ? BuildOptions | BuildOptions.CleanBuildCache : BuildOptions & ~BuildOptions.CleanBuildCache;
+        }
+
+        [Button(space: 10)]
+        private void ChooseBuildPath()
+        {
+            if (string.IsNullOrEmpty(BuildPath))
+            {
+                BuildPath = $"{Application.dataPath}";
+            }
+
+            string newBuildPath = EditorUtility.OpenFolderPanel("Choose Build Path", BuildPath, "Build");
+
+            if (!string.IsNullOrEmpty(newBuildPath))
+            {
+                BuildPath = newBuildPath;
+            }
+        }
+    }
+}
---
+++ Assets/Code/Tools/BuildTool/PlayerBuilder.cs
@@ -0,0 +1,63 @@
+﻿#if UNITY_EDITOR
+
+using System.Linq;
+using UnityEditor;
+
+namespace Tools.BuildTool
+{
+    public static class PlayerBuilder
+    {
+        private static BuildSettings _buildSettings;
+        private static string _locationPathName;
+        private static BuildPlayerOptions _buildPlayerOptions;
+
+        [MenuItem("Tools/Grave/Build Player &b")]
+        private static void Build()
+        {
+            LoadSettingsIfNeeded();
+            SetLocationPathName();
+            SetBuildPlayerOptions();
+
+            BuildPipeline.BuildPlayer(_buildPlayerOptions);
+        }
+
+        private static void LoadSettingsIfNeeded()
+        {
+            if (_buildSettings == null)
+            {
+                _buildSettings = AssetDatabase.LoadAssetAtPath<BuildSettings>("Assets/Settings/BuildSettings.asset");
+            }
+        }
+
+        private static void SetLocationPathName()
+        {
+            string fileName = _buildSettings.BuildTarget switch
+            {
+                BuildTarget.StandaloneWindows or BuildTarget.StandaloneWindows64 => "chess.exe",
+                BuildTarget.Android => "chess.app",
+                BuildTarget.StandaloneOSX => "chess.app",
+                BuildTarget.StandaloneLinux64 => "chess.x86_64",
+                _ => "chess",
+            };
+
+            _locationPathName = _buildSettings.BuildPath + "/" + fileName;
+        }
+
+        private static void SetBuildPlayerOptions()
+        {
+            _buildPlayerOptions = new BuildPlayerOptions
+            {
+                scenes = EditorBuildSettings.scenes
+                    .Where(s => s.enabled)
+                    .Select(s => s.path)
+                    .ToArray(),
+                locationPathName = _locationPathName,
+                targetGroup = _buildSettings.BuildTargetGroup,
+                target = _buildSettings.BuildTarget,
+                options = _buildSettings.BuildOptions,
+            };
+        }
+    }
+}
+
+#endif
