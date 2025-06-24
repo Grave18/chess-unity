a4ada4fe6e385f25acea1aa41807007fca3a4128
Valerii Barabanov <grave63@gmail.com>
2025-06-24 12:31:47 +0300

    Add blend project and samples back

---
+++ Blend/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace chess_3d_blend
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		protected override void OnActivated(EventArgs e)
+		{
+			MainWindow.DataContext = new ViewModel();
+		}
+	}
+}
