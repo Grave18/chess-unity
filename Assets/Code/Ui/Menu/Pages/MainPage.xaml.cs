bb6b84ce0e38ecd81f1a071c3efad191f66950c5
Valerii Barabanov <grave63@gmail.com>
2025-07-11 18:28:43 +0300

    Add popup to Main page

--- Assets/Code/Ui/Menu/Pages/MainPage.xaml.cs
+++ Assets/Code/Ui/Menu/Pages/MainPage.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows;
 using System.Windows.Controls;
 #endif
 
+using System.Windows.Input;
 using Ui.Auxiliary;
 
 namespace Ui.Menu.Pages
@@ -18,7 +19,39 @@ namespace Ui.Menu.Pages
     {
         public MainPage()
         {
+            Initialized += OnInitialized;
             InitializeComponent();
+        }
+
+        private void OnInitialized(object sender, EventArgs e)
+        {
+            SetupKeyBindingToClosePopup();
+        }
+
+        private void SetupKeyBindingToClosePopup()
+        {
+            this.Focusable = true;
+            this.Focus();
+            this.KeyUp += OnKeyUp;
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (!Popup.IsOpen)
+            {
+                return;
+            }
+
+            if (e.Key == Key.Escape)
+            {
+                ClosePopup();
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Enter)
+            {
+                e.Handled = true;
+                Exit();
+            }
         }
 
         public void Play_Click(object sender, RoutedEventArgs args)
@@ -31,12 +64,55 @@ namespace Ui.Menu.Pages
             GameMenuBase.Instance.ChangePage<SettingsPage>();
         }
 
+
         private void Board_Click(object sender, RoutedEventArgs e)
         {
             GameMenuBase.Instance.ChangePage<BoardPage>();
         }
 
-        public void Exit_Click(object sender, RoutedEventArgs args)
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            OpenPopup();
+            SetupPopup();
+        }
+
+        private void UIElement_OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                e.Handled = true;
+                OpenPopup();
+            }
+        }
+
+        private void OpenPopup()
+        {
+            Popup.IsOpen = true;
+        }
+
+        private void SetupPopup()
+        {
+            PopupText.Text = "Are you sure you want to Exit?";
+            PopupYesButton.Content = "Exit";
+            PopupYesButton.Click += PopupYes_Click;
+        }
+
+        private void PopupClose_Click(object sender, RoutedEventArgs e)
+        {
+            ClosePopup();
+        }
+
+        private void ClosePopup()
+        {
+            Popup.IsOpen = false;
+        }
+
+        private void PopupYes_Click(object sender, RoutedEventArgs e)
+        {
+            Exit();
+        }
+
+        public void Exit()
         {
 #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
@@ -48,9 +124,17 @@ namespace Ui.Menu.Pages
         }
 
 #if NOESIS
+        private Popup Popup { get; set; }
+        private TextBlock PopupText { get; set; }
+        private Button PopupYesButton { get; set; }
+
         private void InitializeComponent()
         {
             GUI.LoadComponent(this, "Assets/Code/Ui/Menu/Pages/MainPage.xaml");
+
+            Popup = FindName("Popup") as Popup;
+            PopupText = FindName("PopupText") as TextBlock;
+            PopupYesButton = FindName("PopupYesButton") as Button;
         }
 
         protected override bool ConnectEvent(object source, string eventName, string handlerName)
@@ -76,6 +160,24 @@ namespace Ui.Menu.Pages
             if (eventName == "Click" && handlerName == nameof(Exit_Click))
             {
                 ((Button)source).Click += Exit_Click;
+                return true;
+            }
+
+            if (eventName == "Click" && handlerName == nameof(PopupClose_Click))
+            {
+                ((Button)source).Click += PopupClose_Click;
+                return true;
+            }
+
+            if (eventName == "Click" && handlerName == nameof(PopupYes_Click))
+            {
+                ((Button)source).Click += PopupYes_Click;
+                return true;
+            }
+
+            if (eventName == "KeyUp" && handlerName == nameof(UIElement_OnKeyUp))
+            {
+                ((UIElement)source).KeyUp += UIElement_OnKeyUp;
                 return true;
             }
 
