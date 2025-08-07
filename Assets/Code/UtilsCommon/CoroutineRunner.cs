80fcc74ab98a2e644cfc352512dceff063b6e01c
Valerii Barabanov <grave63@gmail.com>
2025-08-07 21:17:41 +0300

    Add Coroutine runner

---
+++ Assets/Code/UtilsCommon/CoroutineRunner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace UtilsCommon
+{
+    public class CoroutineRunner : MonoBehaviour
+    {
+        private static CoroutineRunner _instance;
+
+        public static CoroutineRunner Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    var coroutineRunnerGo = new GameObject("CoroutineRunner");
+                    _instance = coroutineRunnerGo.AddComponent<CoroutineRunner>();
+                    DontDestroyOnLoad(coroutineRunnerGo);
+                }
+
+                return _instance;
+            }
+        }
+
+        public static Coroutine Run(IEnumerator coroutine)
+        {
+            return Instance.StartCoroutine(coroutine);
+        }
+
+        public static void Stop(Coroutine coroutine)
+        {
+            if (coroutine != null)
+            {
+                Instance.StopCoroutine(coroutine);
+            }
+        }
+    }
+}
