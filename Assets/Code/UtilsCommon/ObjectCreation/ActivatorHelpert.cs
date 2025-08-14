55f9d5ec86f5e8f5ee558fd6e337b5dfc6582bd6
Valerii Barabanov <grave63@gmail.com>
2025-08-14 16:24:45 +0300

    Fix ActivatorHelper

--- Assets/Code/UtilsCommon/ObjectCreation/ActivatorHelpert.cs
+++ Assets/Code/UtilsCommon/ObjectCreation/ActivatorHelpert.cs
@@ -17,13 +17,29 @@ namespace UtilsCommon.ObjectCreation
             }
             catch (Exception e)
             {
-#if UNITY_5_3_OR_NEWER
-                Debug.LogException(e);
-#endif
-                return Activator.CreateInstance<T>();
+                return CreateWithoutParameters<T>(e);
             }
 
             return instance;
+        }
+
+        private static T CreateWithoutParameters<T>(Exception e)
+        {
+#if UNITY_5_3_OR_NEWER
+            Debug.LogException(e);
+#endif
+            try
+            {
+                var instance = (T)Activator.CreateInstance(typeof(T));
+                return instance;
+            }
+            catch (Exception e2)
+            {
+#if UNITY_5_3_OR_NEWER
+                Debug.LogException(e2);
+#endif
+                return default;
+            }
         }
     }
 }
