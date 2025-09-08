2e7315739c4bd902fb587d9c5d00351439ec7957
Valerii Barabanov <grave63@gmail.com>
2025-09-08 14:38:30 +0300

    Add build version on screen

---
+++ Assets/Code/UnityUi/BuildVersionDisplayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace UnityUi
+{
+    public class BuildVersionDisplayer : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void OnGUI()
+        {
+            int w = Screen.width;
+            int h = Screen.height;
+            GUI.skin.label.fontSize = (int)(h * 0.02f);
+
+            string buildVer = "ver: " + Application.version;
+            GUI.Label(new Rect(w * 0.94f, h * 0.95f, h * 0.3f, h * 0.1f), buildVer);
+        }
+    }
+}
