3b48137bc3afb806d485a177993159f644cf25a2
Valerii Barabanov <grave63@gmail.com>
2025-09-08 14:37:02 +0300

    Add post build version increment

---
+++ Assets/Code/UtilsCommon/BuildTool/PostBuildProcessor.cs
@@ -0,0 +1,34 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace UtilsCommon.BuildTool
+{
+    public class PostBuildProcessor : IPostprocessBuildWithReport
+    {
+        public int callbackOrder { get; }
+
+        public void OnPostprocessBuild(BuildReport report)
+        {
+            IncrementBuildVersion();
+            Debug.Log($"Build finished. Version: {PlayerSettings.bundleVersion}");
+        }
+
+        private static void IncrementBuildVersion()
+        {
+            var version = PlayerSettings.bundleVersion;
+            if (!int.TryParse(version, out int versionNumber))
+            {
+                versionNumber = 0;
+            }
+
+            versionNumber += 1;
+            PlayerSettings.bundleVersion = versionNumber.ToString();
+        }
+    }
+}
+
+#endif
