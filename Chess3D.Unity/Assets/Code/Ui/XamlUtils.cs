d44eb93b37bc5a6bc8fc2067c29aca3f16b08d95
Valerii Barabanov <grave63@gmail.com>
2025-11-01 16:51:45 +0300

    Add XamlUtils to load xaml from file path

---
+++ Chess3D.Unity/Assets/Code/Ui/XamlUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace Ui
+{
+    public static class XamlUtils
+    {
+        public static string GetXamlPathFromFilePath([CallerFilePath]string filePath = "")
+        {
+            // Remove .cs
+            string xamlPath = filePath.Replace(".cs", "");
+
+            // Replace \ with /
+            xamlPath = xamlPath.Replace(@"\", "/");
+
+            // Find "Assets/" relative part
+            int assetsIndex = xamlPath.IndexOf("Assets/", StringComparison.OrdinalIgnoreCase);
+            if (assetsIndex == -1)
+            {
+                Debug.LogError($"Cannot locate 'Assets/' in path: {xamlPath}");
+                return "";
+            }
+
+            // Strip to ../Assets/
+            xamlPath = xamlPath.Substring(assetsIndex);
+
+            return xamlPath;
+        }
+    }
+}
