4a86d58e26bdf97d4edc8540e56f1b9a052c6da3
Valerii Barabanov <grave63@gmail.com>
2025-11-02 17:44:14 +0300

    Add Mvvm generator project

---
+++ Chess3D.MvvmToolGenerators/MvvmToolGenerators.Sample/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SourceGenerators.Sample;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        var vm = new ViewModelInterf();
+        vm.TestMethodInterf0();
+
+        Console.WriteLine("Hello, World!");
+        Console.ReadLine();
+    }
+}
---
+++ Chess3D.MvvmToolGenerators/MvvmToolGenerators.Sample/RealyCommandTest.cs
@@ -0,0 +1,61 @@
+﻿using MvvmTool;
+
+namespace SourceGenerators.Sample;
+
+[INotifyPropertyChanged]
+public partial class RelayCommandTest
+{
+    [ObservableProperty]
+    private int _testPropAttr0;
+
+    [ObservableProperty]
+    private int _testPropAttr1;
+
+    [RelayCommand(CanExecute = nameof(TestMethodAttr0_CanExecute))]
+    private void TestMethodAttr0()
+    {
+        TestPropAttr0++;
+        TestPropAttr1++;
+    }
+
+    private bool TestMethodAttr0_CanExecute()
+    {
+        return true;
+    }
+
+    [RelayCommand(CanExecute = nameof(TestMethodAttr1_CanExecute))]
+    private void TestMethodAttr1(object obj)
+    {
+
+    }
+
+    private bool TestMethodAttr1_CanExecute(object obj)
+    {
+        return true;
+    }
+
+    [RelayCommand]
+    private void TestMethodAttr2()
+    {
+
+    }
+
+    [RelayCommand]
+    private void TestMethodAttr3(string str)
+    {
+
+    }
+
+    [RelayCommand(CanExecute = nameof(TestMethodAttr4_CanExecute))]
+    private void TestMethodAttr4()
+    {
+        TestPropAttr0++;
+        TestPropAttr1++;
+    }
+
+    private bool TestMethodAttr4_CanExecute(int i)
+    {
+        return true;
+    }
+
+}
